89800bbfeb2172fd247532a11ce31b64ceb5b9af
Maciek <macrog@users.noreply.github.com>
2023-03-06 16:32:55 +0100

    feat: add mongo connection

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const express = require('express')
 const app = express()
+const {MongoClient} = require('mongodb');
+
+
+async function main(){
+
+    const uri = "mongodb+srv://sana:sana2023@sana.ykms8wg.mongodb.net/test"
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
