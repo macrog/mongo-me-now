e35321a531739c2e6a4a7bc85bf07624fd015ec9
Maciek Gorczynski <maciekg81@gmail.com>
2023-03-08 15:36:01 +0100

    feat: provide mongodb uri as variables

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb+srv://sana:sana2023@sana.ykms8wg.mongodb.net/clinic", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(
+        `${process.env.MONGO_URI_1}//${process.env.USER}:${process.env.PASSWORD}${process.env.MONGO_URI_2}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
     .then(() => {
         console.log("Connected to MongoDB");
     })
     .catch((error) => {
         console.log("Unable to connect to MongoDB", error);
     });
+
+// export USER=sana
+// export PASSWORD=sana2023
+// export MONGO_URI_1=mongodb+srv:
+// export MONGO_URI_2=@sana.ykms8wg.mongodb.net/clinic
