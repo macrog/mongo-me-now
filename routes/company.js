db29327b85b4ad4f087324bd221f409b76be308e
Maciek Gorczynski <maciekg81@gmail.com>
2024-02-07 14:36:02 +0100

    feat: add appointment endpoint

--- routes/company.js
+++ routes/company.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const axios = require("axios");
 const Company = require("../models/company");
+
+const bookingApi = "https://user-api-v2.simplybook.me/admin";
 
 const router = new express.Router();
 
@@ -7,6 +10,37 @@ router.get("/company", async (req, res) => {
     try {
         const company = await Company.find({});
         res.status(200).send(company[0]);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+router.get("/appointment", async (req, res) => {
+    try {
+        axios
+            .post(bookingApi + "/auth", {
+                company: "fakecompany125",
+                login: "maciekg81@gmail.com",
+                password: "^3NofG9z%wzrFV1J",
+            })
+            .then((response) => {
+                axios
+                    .get(bookingApi + "/bookings?filter[upcoming_only]=1", {
+                        headers: {
+                            "X-Company-Login": "fakecompany125",
+                            "X-Token": response.data.token,
+                        },
+                    })
+                    .then((response) => {
+                        res.status(200).send(response.data);
+                    })
+                    .catch((error) => {
+                        res.status(500).send(error);
+                    });
+            })
+            .catch((error) => {
+                res.status(500).send(error);
+            });
     } catch (error) {
         res.status(500).send(error);
     }
