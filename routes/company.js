12d6a4653259d4b17a28ecc87e9a172efefe994f
Maciek Gorczynski <maciekg81@gmail.com>
2024-02-12 11:36:54 +0100

    feat: add cors headers

--- routes/company.js
+++ routes/company.js
@@ -29,6 +29,9 @@ router.get("/appointment", async (req, res) => {
                         headers: {
                             "X-Company-Login": "fakecompany125",
                             "X-Token": response.data.token,
+                            "Access-Control-Allow-Origin": "x",
+                            "Access-Control-Allow-Headers":
+                                "Origin, X-Requested-With, Content-Type, Accept",
                         },
                     })
                     .then((response) => {
